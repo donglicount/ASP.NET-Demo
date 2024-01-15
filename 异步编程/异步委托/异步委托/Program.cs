a09a851ee328d9bbb3755071094cfac03e3f7ceb
donglicount <756854672@qq.com>
2024-01-15 20:51:06 +0800

    异步委托

---
+++ 异步编程/异步委托/异步委托/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace 异步委托
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadPool.QueueUserWorkItem(async (obj) =>
+            {
+                while (true)
+                {
+                   await File.WriteAllTextAsync(@"d:\a\1.txt","aaaaaaaaaaaaaaaaa");
+                   Console.WriteLine("xxxxx");
+                }
+            });
+            Console.Read();
+
+            //在lamad表达式中使用异步方法，只需要在前面加上async关键字
+        }
+    }
+}
