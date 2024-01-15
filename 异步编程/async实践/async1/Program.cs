ac95ee31c6003a7d60f077d5ed2f63d6eb81b6da
donglicount <756854672@qq.com>
2024-01-15 20:19:19 +0800

    异步方法实践

---
+++ 异步编程/async实践/async1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace async1
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int length= await DownloadHtmlAsync("http://www.youzack.com", @"d:\a\1.txt");
+            Console.WriteLine("html长度："+length);
+        }
+
+        // static async Task DownloadHtmlAsync(string url,string filename)
+        // {
+        //     //using关键字：对于实现了IDisposable接口的需要使用using进行资源回收
+        //     using (HttpClient client = new HttpClient())
+        //     {
+        //         string html=await client.GetStringAsync(url);
+        //         File.WriteAllTextAsync(filename, html);
+        //     }
+        //
+        // }
+        static async Task<int> DownloadHtmlAsync(string url, string filename)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string html = await client.GetStringAsync(url);
+                File.WriteAllTextAsync(filename, html);
+                return html.Length;
+            }
+        }
+    }
+}
