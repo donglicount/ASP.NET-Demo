08cb85630a8886fb3f5cd50b9b9ce67d23ff4d2e
donglicount <756854672@qq.com>
2024-01-15 21:15:50 +0800

    await，async的原理：使用ILSpy反编译后发现，await被拆分成多个状态，进行切换，实际上代码还是在顺序执行。

---
+++ 异步编程/await和async的原理/asyncawait原理/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace asyncawait原理
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (HttpClient httpClient=new HttpClient())
+            {
+                string html = await httpClient.GetStringAsync("http://www.taobao.com");
+                Console.WriteLine(html);
+            }
+
+            string txt = "hello";
+            string filename = @"d:\a\1.txt";
+            await File.WriteAllTextAsync(filename, txt);
+            Console.WriteLine("写入成功！");
+            string s = await File.ReadAllTextAsync(filename);
+            Console.WriteLine("读取："+s);
+
+
+            //await async 是语法糖,最终会编译成状态机模式，await的方法会被编译成一个类，根据调用切分
+            //为多个状态，对async方法的调用会被拆分为对MoveNext的调用。
+            //用await看似是”等待“，经过编译后，其实没有”await“
+        }
+    }
+}
