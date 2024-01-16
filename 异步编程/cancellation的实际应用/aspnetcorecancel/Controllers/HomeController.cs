a49706a647c5807bedcccf7f116a4af9022e6285
‘donglee’ <756854672@qq.com>
2024-01-16 15:17:06 +0800

    cancellationtoken在aspnetcore中的实际使用

---
+++ 异步编程/cancellation的实际应用/aspnetcorecancel/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using aspnetcorecancel.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace aspnetcorecancel.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index(CancellationToken cancellationToken)
+        {
+            await DownloadAsync3("http://www.youzack.com", 10000, cancellationToken);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        static async Task DownloadAsync3(string url, int n, CancellationToken cancellationToken)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    var resp = await httpClient.GetAsync(url, cancellationToken);
+                    string html = await resp.Content.ReadAsStringAsync();
+                    Debug.WriteLine(html);
+                }
+            }
+        }
+    }
+}
---
+++ 异步编程/cancellation的实际应用/aspnetcorecancel/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace aspnetcorecancel.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
