3f56b52f5c8708d8a859062aa4c4cb6e3c70b61d
‘donglee’ <756854672@qq.com>
2024-01-16 11:12:04 +0800

    异步sleep

---
+++ 异步编程/异步sleep/asyncsleep/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace asyncsleep
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            using (HttpClient http=new HttpClient())
+            {
+                string s=await http.GetStringAsync("http://www.youzack.com");
+                textBox1.Text = s;
+                await Task.Delay(3000);
+                string s2=await http.GetStringAsync("http://www.baidu.com");
+                textBox1.Text = s2;
+            }
+        }
+    }
+}
